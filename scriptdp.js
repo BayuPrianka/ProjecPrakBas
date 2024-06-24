4ba0b5ce71bb37b468115eb2111827424c067c12
BayuPrianka <bayuprianka118@gmail.com>
2024-06-24 17:26:57 +0800

    a

--- scriptdp.js
+++ scriptdp.js
@@ -1,54 +1,54 @@
 document.addEventListener("DOMContentLoaded", () => {
     const subMenuWrap = document.getElementById("subMenuWrap");
     const profilGambar = document.querySelector(".profilgambarmenu");
-
+  
     function toggleMenu() {
         subMenuWrap.classList.toggle("open-menu");
     }
-
+  
     profilGambar.addEventListener("click", (e) => {
         e.stopPropagation();
         toggleMenu();
     });
-
+  
     document.addEventListener("click", (e) => {
         if (!subMenuWrap.contains(e.target) && subMenuWrap.classList.contains("open-menu")) {
             subMenuWrap.classList.remove("open-menu");
         }
     });
-
+  
     const modal = document.getElementById("myModal");
     const btn = document.getElementById("openModalBtn");
-
+  
     // Open the modal
     btn.onclick = () => {
         modal.style.display = "block";
     }
-
+  
     // Close the modal when clicking outside of the modal content
     window.onclick = (event) => {
         if (event.target == modal) {
             modal.style.display = "none";
         }
     }
-});
-
-// Sidebar
-const body = document.querySelector("body");
-const sidebar = body.querySelector(".sidebar");
-const contenIfSidebarOpen = body.querySelector(".content");
-const toggle = body.querySelector(".menu-utama");
-
-// Variabel untuk melacak apakah tombol menu utama telah ditekan
-let menuUtamaToggled = false;
-
-toggle.addEventListener("click", () => {
+  });
+  
+  // Sidebar
+  const body = document.querySelector("body");
+  const sidebar = body.querySelector(".sidebar");
+  const contenIfSidebarOpen = body.querySelector(".content");
+  const toggle = body.querySelector(".menu-utama");
+  
+  // Variabel untuk melacak apakah tombol menu utama telah ditekan
+  let menuUtamaToggled = false;
+  
+  toggle.addEventListener("click", () => {
     // Toggle kelas 'close' pada sidebar
     sidebar.classList.toggle("close");
     contenIfSidebarOpen.classList.toggle("ori");
     // Update status menu utama
     menuUtamaToggled = !menuUtamaToggled;
-
+  
     // Aktifkan atau nonaktifkan event listener berdasarkan status menu utama
     if (menuUtamaToggled) {
         // Nonaktifkan event listener
@@ -59,30 +59,33 @@ toggle.addEventListener("click", () => {
         sidebarauto.addEventListener('mouseover', openSidebar);
         sidebarauto.addEventListener('mouseout', closeSidebar);
     }
-});
-
-// Mendapatkan elemen sidebar
-let sidebarauto = document.getElementById('mySidebar');
-let contenauto = document.getElementById('content');
-
-// Fungsi untuk membuka sidebar
-function openSidebar() {
+  });
+  
+  // Mendapatkan elemen sidebar
+  let sidebarauto = document.getElementById('mySidebar');
+  let contenauto = document.getElementById('content');
+  let baratasauto = document.getElementById('barAtas');
+  
+  // Fungsi untuk membuka sidebar
+  function openSidebar() {
     sidebarauto.classList.remove('close');
     contenauto.classList.remove('ori');
-}
-
-// Fungsi untuk menutup sidebar
-function closeSidebar() {
+    baratasauto.classList.remove('ori');
+  }
+  
+  // Fungsi untuk menutup sidebar
+  function closeSidebar() {
     contenauto.classList.add('ori');
+    baratasauto.classList.add('ori');
     sidebarauto.classList.add('close');
-}
-
-// Menambahkan event listener untuk mouseover
-sidebarauto.addEventListener('mouseover', openSidebar);
-
-// Menambahkan event listener untuk mouseout
-sidebarauto.addEventListener('mouseout', closeSidebar);
-
+  }
+  
+  // Menambahkan event listener untuk mouseover
+  sidebarauto.addEventListener('mouseover', openSidebar);
+  
+  // Menambahkan event listener untuk mouseout
+  sidebarauto.addEventListener('mouseout', closeSidebar);
+  
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { 
     getFirestore,
